9f7e114f7156143e77f069f7264c94d3367bce0d
jacoapstory <44868579+jacoapstory@users.noreply.github.com>
2025-01-23 16:24:35 +0200

    Added Visual Studio Extension Project Framework

---
+++ App/Apstory.Scaffold.VisualStudio/Window/ScaffoldWindow.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Apstory.Scaffold.VisualStudio.Window
+{
+    /// <summary>
+    /// This class implements the tool window exposed by this package and hosts a user control.
+    /// </summary>
+    /// <remarks>
+    /// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane,
+    /// usually implemented by the package implementer.
+    /// <para>
+    /// This class derives from the ToolWindowPane class provided from the MPF in order to use its
+    /// implementation of the IVsUIElementPane interface.
+    /// </para>
+    /// </remarks>
+    [Guid("91f02b66-136b-4de7-b383-ffb43620868b")]
+    public class ScaffoldWindow : ToolWindowPane
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ScaffoldWindow"/> class.
+        /// </summary>
+        public ScaffoldWindow() : base(null)
+        {
+            this.Caption = "Apstory Scaffolding";
+
+            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
+            // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
+            // the object returned by the Content property.
+            this.Content = new ScaffoldWindowControl();
+        }
+    }
+}
---
+++ App/Apstory.Scaffold.VisualStudio/Window/ScaffoldWindowControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Apstory.Scaffold.VisualStudio.Window
+{
+    /// <summary>
+    /// Interaction logic for ScaffoldWindowControl.
+    /// </summary>
+    public partial class ScaffoldWindowControl : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ScaffoldWindowControl"/> class.
+        /// </summary>
+        public ScaffoldWindowControl()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handles click on the button by displaying a message box.
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event args.</param>
+        [SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
+                "ScaffoldWindow");
+        }
+    }
+}
