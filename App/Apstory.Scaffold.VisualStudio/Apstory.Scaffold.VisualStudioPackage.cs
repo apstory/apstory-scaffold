8faaf54a6205ea1d1c8a91bf12f4202ffd9a1bc0
JacoBijker <145118031+JacoBijker@users.noreply.github.com>
2025-02-09 20:57:20 +0200

    Created Visual Studio Apstory Scaffold toolbar

--- App/Apstory.Scaffold.VisualStudio/Apstory.Scaffold.VisualStudioPackage.cs
+++ App/Apstory.Scaffold.VisualStudio/Apstory.Scaffold.VisualStudioPackage.cs
@@ -2,9 +2,11 @@
 using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.TextManager.Interop;
 using System;
 using System.ComponentModel.Design;
 using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading;
 using Task = System.Threading.Tasks.Task;
 
@@ -38,6 +40,9 @@ namespace Apstory.Scaffold.VisualStudio
         /// </summary>
         public const string PackageGuidString = "033376d0-4630-4068-89d7-e6629cfe6645";
 
+        private const string guidApstoryScaffoldVisualStudioPackageCmdSet = "bf130a03-5202-4448-8173-02f6b1d00bd2"; // Match `guidApstoryScaffoldVisualStudioPackageCmdSet`
+        private const int ToolbarTestCommandId = 0x1051; // Matches `ToolbarTestCommandId`
+
         #region Package Members
 
         /// <summary>
@@ -53,8 +58,139 @@ namespace Apstory.Scaffold.VisualStudio
             // Do any initialization that requires the UI thread after switching to the UI thread.
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
 
-            await ScaffoldWindowCommand.InitializeAsync(this);
+            OleMenuCommandService commandService = await GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (commandService != null)
+            {
+                var cmdId = new CommandID(new Guid(guidApstoryScaffoldVisualStudioPackageCmdSet), ToolbarTestCommandId);
+                var menuItem = new MenuCommand(ExecuteCommand, cmdId);
+                commandService.AddCommand(menuItem);
+            }
         }
+
+        private void ExecuteCommand(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            WriteToOutputWindow("Dude, you executed something");
+
+            GetActiveFileContent();
+            //VsShellUtilities.ShowMessageBox(
+            //    this,
+            //    "Scaffold Code Button Clicked!",
+            //    "Information",
+            //    OLEMSGICON.OLEMSGICON_INFO,
+            //    OLEMSGBUTTON.OLEMSGBUTTON_OK,
+            //    OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+
+        private void GetActiveFileContent()
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            // Get the active text view
+            var textManager = (IVsTextManager)this.GetService(typeof(SVsTextManager));
+            if (textManager == null) return;
+
+            textManager.GetActiveView(1, null, out IVsTextView textView);
+
+            if (textView != null)
+            {
+                var path = GetFilePathFromTextView(textView);
+                WriteToOutputWindow($"Current open view file: {path}");
+
+                // Get the buffer associated with the text view
+                textView.GetBuffer(out IVsTextLines textLines);
+
+                if (textLines != null)
+                {
+                    // Get the number of lines in the buffer
+                    textLines.GetLineCount(out int lineCount);
+
+                    WriteToOutputWindow($"Lines: {lineCount}");
+
+                    StringBuilder fileContent = new StringBuilder();
+
+                    // Iterate through each line and get the text
+                    for (int lineNum = 0; lineNum < lineCount; lineNum++)
+                    {
+                        textLines.GetLineText(lineNum, 0, lineNum, int.MaxValue, out string lineText);
+                        fileContent.AppendLine(lineText);
+                    }
+
+                    // Write the content of the file to the Output window
+                    WriteToOutputWindow($"Currently open file content: {fileContent}");
+                }
+            }
+        }
+
+        private void WriteToOutputWindow(string message)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            // Get the Output window service
+            var outputWindow = (IVsOutputWindow)this.GetService(typeof(SVsOutputWindow));
+            if (outputWindow == null)
+                return;
+
+            // Create a new pane or get the existing one
+            Guid generalPaneGuid = VSConstants.GUID_OutWindowGeneralPane;
+            outputWindow.GetPane(ref generalPaneGuid, out IVsOutputWindowPane outputPane);
+
+            if (outputPane == null)
+            {
+                outputWindow.CreatePane(ref generalPaneGuid, "Apstory Scaffold", 1, 1);
+                outputWindow.GetPane(ref generalPaneGuid, out outputPane);
+            }
+
+            // Write message to the pane
+            outputPane.OutputString(message + "\n");
+        }
+
+        private string GetFilePathFromTextView(IVsTextView textView)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            // Get the IVsRunningDocumentTable service
+            var rdt = (IVsRunningDocumentTable)this.GetService(typeof(SVsRunningDocumentTable));
+            if (rdt == null)
+            {
+                return "Unable to retrieve running document table";
+            }
+
+            uint docCookie = 0;
+            string filePath = null;
+            IVsHierarchy hierarchy = null;
+            uint itemId = 0;
+            uint rdtFlags = 0;
+            uint readOnly = 0;
+            uint editlock = 0;
+            IntPtr docData = IntPtr.Zero;
+
+            // GetDocumentInfo requires a docCookie to be filled
+            int result = rdt.GetDocumentInfo(
+                docCookie,
+                out rdtFlags,
+                out readOnly,
+                out editlock,
+                out filePath,
+                out hierarchy,
+                out itemId,
+                out docData);
+
+            // Debugging output
+            if (result != VSConstants.S_OK)
+            {
+                return $"Error retrieving document info. Result: {result} ({result.ToString("X8")})";
+            }
+
+            if (string.IsNullOrEmpty(filePath))
+            {
+                return "File path is empty or null";
+            }
+
+            return filePath;
+        }
+
+
         #endregion
     }
 }
