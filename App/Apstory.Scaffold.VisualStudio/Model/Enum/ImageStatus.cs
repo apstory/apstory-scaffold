3967dfe028346993f46535e768d0fffcbe8d49da
jacoapstory <44868579+jacoapstory@users.noreply.github.com>
2025-02-19 16:58:48 +0200

    Added ImageStatus Enum

---
+++ App/Apstory.Scaffold.VisualStudio/Model/Enum/ImageStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace Apstory.Scaffold.VisualStudio.Model.Enum
+{
+    public enum ImageStatus : byte
+    {
+        Normal = 1,
+        Fail = 2,
+        Skipped = 3,
+        Success = 4
+    }
+}
